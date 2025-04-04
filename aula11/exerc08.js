2548f4880f0fcb5124973cec191f7634c6f9d1bf
BrunoHenri1 <olivierabruno16@gmail.com>
2025-04-04 11:26:33 -0300

    Estudos JS

---
+++ aula11/exerc08.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`a Velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){
+    console.log(`Voce ultrapassou o limite de Velocidade! MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
+
+
