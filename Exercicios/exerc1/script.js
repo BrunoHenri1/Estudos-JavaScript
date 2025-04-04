d04d5bd5fa22b8ec0d0ad3308c87db28e775a8a7
BrunoHenri1 <olivierabruno16@gmail.com>
2025-04-04 18:25:05 -0300

    Estudos JS

--- Exercicios/exerc1/script.js
+++ Exercicios/exerc1/script.js
@@ -1,2 +1,21 @@
-var agora = new Date()
-var hora = agora.getHours()
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    hora = 19
+    msg.innerHTML = (`Agora são ${hora} horas`)
+    if (hora > 6 && hora < 12){
+        img.src = "imagens/manhã.png"
+        document.body.style.background = "#6b7195"
+        
+    } else if (hora >= 12 &&hora <= 18){
+        img.src = "imagens/tarde.png"
+        document.body.style.background = "#f2b66f"
+
+    } else {
+        img.src = "imagens/noite.png"
+        document.body.style.background = "#1b233b"
+    }
+    
+}
