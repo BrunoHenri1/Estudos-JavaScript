12a2b74657a1c1d950ad4ac3606e2356676725c3
BrunoHenri1 <olivierabruno16@gmail.com>
2025-09-15 17:53:41 -0300

    Update script.js

--- aulaRB/aula2/script.js
+++ aulaRB/aula2/script.js
@@ -1,23 +1,29 @@
 function adicionarTarefa() {
-        //mostra a mensagem quando clica no botão
-        let mensagem = "Tarefa adicionada com sucesso!";
-        
         //Recebe o valor do usuario
         let inputTarefa = document.getElementById("inputTarefa")
         //atribui o valor do usuario a variavel tarefa e mostra em baixo
-        let Tarefa = inputTarefa.value
-            document.getElementById("mensagem").textContent = mensagem;
+        let Tarefa = inputTarefa.value.trim()
 
-        //cria uma lista de tarefas (li) e atribui os valores numa lista não ordenada (ul)
-        let listaTarefas = document.getElementById("listaTarefas")
-        let novaTarefa = document.createElement("li")
+        let mensagem = document.getElementById("mensagem")
+            
+        
+        if (Tarefa == "") {
+            let mensagemErro = "Erro voce não digitou nada"
+            mensagem.textContent = mensagemErro;
+        } else {
+            //mostra a mensagem quando clica no botão
+            let mensagemSucesso = "Tarefa adicionada com sucesso!";
+            mensagem.textContent = mensagemSucesso;
 
-        //as novas tarefas recebem o valor digitado do input
-        novaTarefa.textContent = Tarefa
+            //cria uma lista de tarefas (li) e atribui os valores numa lista não ordenada (ul)
+            let listaTarefas = document.getElementById("listaTarefas")
+            let novaTarefa = document.createElement("li")
+             //as novas tarefas recebem o valor digitado do input
+            novaTarefa.textContent = Tarefa
 
-        //adiciona a tarefa na li
-        listaTarefas.appendChild(novaTarefa)
-
+            //adiciona a tarefa na li
+            listaTarefas.appendChild(novaTarefa)
+        }
         //limpa o input do usuario
         inputTarefa.value = ""
     }
