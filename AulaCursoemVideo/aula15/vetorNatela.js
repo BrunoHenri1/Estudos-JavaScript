37bcf3795fe71ffd164dde42133970fccdcc968a
BrunoHenri1 <olivierabruno16@gmail.com>
2026-01-15 15:03:47 -0300

    update Vetor

--- AulaCursoemVideo/aula15/vetorNatela.js
+++ AulaCursoemVideo/aula15/vetorNatela.js
@@ -1,5 +1,9 @@
 let valores = [8,1,7,4,2,9]
 
-for (let pos=0; valores.length > pos; pos++){
+/*for (let pos=0; valores.length > pos; pos++){
+    console.log(`O vetor na posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+
+for (let pos in valores){
     console.log(`O vetor na posição ${pos} tem o valor ${valores[pos]}`)
 }
