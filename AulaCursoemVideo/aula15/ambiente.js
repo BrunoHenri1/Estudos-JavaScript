de8dd0887786679296621f7251bb2896eee098c3
BrunoHenri1 <olivierabruno16@gmail.com>
2026-01-15 14:59:13 -0300

    Vetor

---
+++ AulaCursoemVideo/aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,3,4,9]
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O primeiro vetor do valor é ${num[0]}`)
+console.log(`Nosso vetor tem o tamanho de ${num.length} indices, antes de adicionar outro valor!`)
+num.push(1)
+console.log(`O vetor em ordem crescente é ${num.sort()}`)
+
+
---
+++ AulaCursoemVideo/aula15/vetorNatela.js
@@ -0,0 +1,5 @@
+let valores = [8,1,7,4,2,9]
+
+for (let pos=0; valores.length > pos; pos++){
+    console.log(valores[pos])
+}
