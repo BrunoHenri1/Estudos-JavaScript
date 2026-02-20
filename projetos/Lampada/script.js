30414ba4978454e66f1ffb0e2bb6d263f3cdda52
BrunoHenri1 <olivierabruno16@gmail.com>
2026-02-20 15:34:38 -0300

    update

--- projetos/Lampada/script.js
+++ projetos/Lampada/script.js
@@ -1,15 +1,33 @@
 let img = document.getElementById('lamp')
 const ligar = document.getElementById('ligar')
 const desligar = document.getElementById('desligar')
-
-ligar.addEventListener('click', function(){
-    img.src = "img/ligada.jpg"
-});
-
-desligar.addEventListener('click', function(){
-    img.src = 'img/desligada.jpg'
-})
+let quebrada = false
 
 img.addEventListener('dblclick', function(){
     img.src = 'img/quebrada.jpg'
+    quebrada = true
+})
+
+ligar.addEventListener('click', function(){
+    if (!quebrada){
+        img.src = "img/ligada.jpg"
+    }
+});
+
+desligar.addEventListener('click', function(){
+    if (!quebrada){
+        img.src = 'img/desligada.jpg'
+    }
+})
+
+img.addEventListener('mouseenter', function(){
+    if (!quebrada){
+        img.src = 'img/ligada.jpg'
+    }   
+})
+
+img.addEventListener('mouseout', function(){
+    if (!quebrada){
+        img.src = 'img/desligada.jpg'
+    }
 })
